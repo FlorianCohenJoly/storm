c058e557651136b12853b390e4cdcffe6c988cb1
FlorianCohenJoly <floriancohenjoy@gmail.com>
2025-02-21 11:18:41 +0100

    push final

--- examples/storm_examples/try.py
+++ examples/storm_examples/try.py
@@ -12,6 +12,10 @@ from knowledge_storm.rm import VectorRM
 from knowledge_storm.lm import OllamaClient  # Use OllamaClient for local Docker image
 from knowledge_storm.utils import load_api_key, QdrantVectorStoreManager
 
+def truncate_prompt(prompt, limit=1024):
+    """Tronque le prompt pour respecter la limite de contexte."""
+    tokens = prompt.split()  # Simple split sur les espaces (peut être amélioré avec un tokenizer)
+    return " ".join(tokens[:limit])
 
 def main(args):
     load_api_key(toml_file_path=r"examples\storm_examples\secrets.toml")  # Load any necessary API keys
@@ -29,7 +33,7 @@ def main(args):
     question_asker_lm = OllamaClient(max_tokens=500, **ollama_kwargs)
     outline_gen_lm = OllamaClient(max_tokens=400, **ollama_kwargs)
     article_gen_lm = OllamaClient(max_tokens=700, **ollama_kwargs)
-    article_polish_lm = OllamaClient(max_tokens=4000, **ollama_kwargs)
+    article_polish_lm = OllamaClient(max_tokens=2000, **ollama_kwargs)
 
     lm_configs.set_conv_simulator_lm(conv_simulator_lm)
     lm_configs.set_question_asker_lm(question_asker_lm)
@@ -57,7 +61,8 @@ def main(args):
         qdrant_api_key=os.getenv("QDRANT_API_KEY"),
         embedding_model=args.embedding_model,
         device=args.device,
-        batch_size=args.embed_batch_size,
+        batch_size=args.embed_batch_size
+
     )
 
     rm = VectorRM(
@@ -143,130 +148,26 @@ def main(args):
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    # global arguments
-    parser.add_argument(
-        "--url", type=str, default="http://localhost", help="URL of the Ollama server."
-    )
-    parser.add_argument(
-        "--port", type=int, default=11434, help="Port of the Ollama server."
-    )
-    parser.add_argument(
-        "--model", type=str, default="llama3.2:latest", help="Model of the Ollama server."
-    )
-    parser.add_argument(
-        "--output-dir",
-        type=str,
-        default="./results/gpt_retrieval",
-        help="Directory to store the outputs.",
-    )
-    parser.add_argument(
-        "--max-thread-num",
-        type=int,
-        default=3,
-        help="Maximum number of threads to use. The information seeking part and the article generation"
-        "part can speed up by using multiple threads. Consider reducing it if keep getting "
-        '"Exceed rate limit" error when calling LM API.',
-    )
-    # provide local corpus and set up vector db
-    parser.add_argument(
-        "--collection-name",
-        type=str,
-        default="my_documents",
-        help="The collection name for vector store.",
-    )
-    parser.add_argument(
-        "--embedding_model",
-        type=str,
-        default="BAAI/bge-m3",
-        help="The collection name for vector store.",
-    )
-    parser.add_argument(
-        "--device",
-        type=str,
-        default="cpu",
-        help="The device used to run the retrieval model (mps, cuda, cpu, etc).",
-    )
-    parser.add_argument(
-        "--vector-db-mode",
-        type=str,
-        choices=["offline", "online"],
-        help="The mode of the Qdrant vector store (offline or online).",
-    )
-    parser.add_argument(
-        "--offline-vector-db-dir",
-        type=str,
-        default="./vector_store",
-        help="If use offline mode, please provide the directory to store the vector store.",
-    )
-    parser.add_argument(
-        "--online-vector-db-url",
-        type=str,
-        help="If use online mode, please provide the url of the Qdrant server.",
-    )
-    parser.add_argument(
-        "--csv-file-path",
-        type=str,
-        default=None,
-        help="The path of the custom document corpus in CSV format. The CSV file should include "
-        "content, title, url, and description columns.",
-    )
-    parser.add_argument(
-        "--embed-batch-size",
-        type=int,
-        default=64,
-        help="Batch size for embedding the documents in the csv file.",
-    )
-    # stage of the pipeline
-    parser.add_argument(
-        "--do-research",
-        action="store_true",
-        help="If True, simulate conversation to research the topic; otherwise, load the results.",
-    )
-    parser.add_argument(
-        "--do-generate-outline",
-        action="store_true",
-        help="If True, generate an outline for the topic; otherwise, load the results.",
-    )
-    parser.add_argument(
-        "--do-generate-article",
-        action="store_true",
-        help="If True, generate an article for the topic; otherwise, load the results.",
-    )
-    parser.add_argument(
-        "--do-polish-article",
-        action="store_true",
-        help="If True, polish the article by adding a summarization section and (optionally) removing "
-        "duplicate content.",
-    )
-    # hyperparameters for the pre-writing stage
-    parser.add_argument(
-        "--max-conv-turn",
-        type=int,
-        default=3,
-        help="Maximum number of questions in conversational question asking.",
-    )
-    parser.add_argument(
-        "--max-perspective",
-        type=int,
-        default=3,
-        help="Maximum number of perspectives to consider in perspective-guided question asking.",
-    )
-    parser.add_argument(
-        "--search-top-k",
-        type=int,
-        default=3,
-        help="Top k search results to consider for each search query.",
-    )
-    # hyperparameters for the writing stage
-    parser.add_argument(
-        "--retrieve-top-k",
-        type=int,
-        default=3,
-        help="Top k collected references for each section title.",
-    )
-    parser.add_argument(
-        "--remove-duplicate",
-        action="store_true",
-        help="If True, remove duplicate content from the article.",
-    )
+    parser.add_argument("--url", type=str, default="http://localhost")
+    parser.add_argument("--port", type=int, default=11434)
+    parser.add_argument("--model", type=str, default="llama3.2:latest")
+    parser.add_argument("--output-dir", type=str, default="./results/gpt_retrieval")
+    parser.add_argument("--max-thread-num", type=int, default=2)  # Réduction des threads pour éviter surcharge
+    parser.add_argument("--collection-name", type=str, default="my_documents")
+    parser.add_argument("--embedding_model", type=str, default="BAAI/bge-m3")
+    parser.add_argument("--device", type=str, default="cpu")
+    parser.add_argument("--vector-db-mode", type=str, choices=["offline", "online"])
+    parser.add_argument("--offline-vector-db-dir", type=str, default="./vector_store")
+    parser.add_argument("--online-vector-db-url", type=str)
+    parser.add_argument("--csv-file-path", type=str, default=None)
+    parser.add_argument("--embed-batch-size", type=int, default=32)  # Réduction batch size
+    parser.add_argument("--do-research", action="store_true")
+    parser.add_argument("--do-generate-outline", action="store_true")
+    parser.add_argument("--do-generate-article", action="store_true")
+    parser.add_argument("--do-polish-article", action="store_true")
+    parser.add_argument("--max-conv-turn", type=int, default=3)
+    parser.add_argument("--max-perspective", type=int, default=3)
+    parser.add_argument("--search-top-k", type=int, default=3)
+    parser.add_argument("--retrieve-top-k", type=int, default=3)
+    parser.add_argument("--remove-duplicate", action="store_true")
     main(parser.parse_args())
